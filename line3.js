b09eb3cde2fb8ddbeecb32984aefe0fe1e74f280
Firejoust <42525540+Camezza@users.noreply.github.com>
2021-08-15 17:28:29 +1000

    Rename functions, implement line rotation

--- line3.js
+++ line3.js
@@ -10,41 +10,61 @@ class Line3 {
         return new Line3(this.a.x + x, this.a.y + y, this.a.z + z, this.b.x + x, this.b.y + y, this.b.z + z);
     }
 
-    translate(x, y, z) {
+    setOffset(x, y, z) {
         this.a.add(x, y, z);
         this.b.add(x, y, z);
     }
 
-    minus(x, y, z) {
+    difference(x, y, z) {
         return new Line3(this.a.x - x, this.a.y - y, this.a.z - z, this.b.x - x, this.b.y - y, this.b.z - z);
     }
 
-    subtract(x, y, z) {
+    setDifference(x, y, z) {
         this.a.subtract(x, y, z);
         this.b.subtract(x, y, z);
     }
 
-    multiply(x, y, z) {
+    dilate(x, y, z) {
         return new Line3(this.a.x * x, this.a.y * y, this.a.z * z, this.b.x * x, this.b.y * y, this.b.z * z);
     }
 
-    dilate(x, y, z) {
+    setDilate(x, y, z) {
         this.a.multiply(x, y, z);
         this.b.multiply(x, y, z);
     }
 
-    divide(x, y, z) {
+    compress(x, y, z) {
         return new Line3(this.a.x / x, this.a.y / y, this.a.z / z, this.b.x / x, this.b.y / y, this.b.z / z);
     }
 
-    compress(x, y, z) {
+    setCompress(x, y, z) {
         this.a.divide(x, y, z);
         this.b.divide(x, y, z);
     }
+
     // acts as a circular normal, shifting point B by a specified angle
-    rotate(yaw, pitch);
+    rotate(yaw, pitch) {
+        let hRad = Math.sqrt((this.b.z - this.a.z) ** 2 + (this.b.x - this.a.x) ** 2);
+        let vRad = Math.sqrt((this.b.y - this.a.y) ** 2 + (this.b.x - this.a.x) ** 2);
+        let hAngle = Math.atan2((this.b.z - this.a.z), (this.b.x - this.a.x)) + yaw;
+        let vAngle = Math.atan2((this.b.y - this.a.y), (this.b.x - this.a.x)) + pitch;
+        return new Line3(this.a.x, this.a.y, this.a.z, Math.cos(hAngle) * hRad, Math.sin(vAngle) * vRad, Math.sin(hAngle) * hRad);
+    }
+
+    setRotate(yaw, pitch) {
+        let hRad = Math.sqrt((this.b.z - this.a.z) ** 2 + (this.b.x - this.a.x) ** 2);
+        let vRad = Math.sqrt((this.b.y - this.a.y) ** 2 + (this.b.x - this.a.x) ** 2);
+        let hAngle = Math.atan2((this.b.z - this.a.z), (this.b.x - this.a.x)) + yaw;
+        let vAngle = Math.atan2((this.b.y - this.a.y), (this.b.x - this.a.x)) + pitch;
+        this.b.x = Math.cos(hAngle) * hRad;
+        this.b.y = Math.sin(vAngle) * vRad;
+        this.b.z = Math.sin(hAngle) * hRad;
+    }
+
     // intercept with another 3d line
-    intercept(line);
+    intercept(line) {
+
+    }
 }
 
 module.exports = Line3;
